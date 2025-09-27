afcb03ad5115d6d5a67a374a90cd04cd164dfbd7
FreddySnzz <fredsonsantos@seduc.pi.gov.br>
2025-09-27 03:56:39 -0300

    fix: fixed build errors

--- src/app/cards/page.tsx
+++ src/app/cards/page.tsx
@@ -1,9 +1,9 @@
 import Cards from "@/components/cards/Cards";
 import LayoutContainer from "@/components/layout/LayoutContainer";
 
-export default function CardsPage({ searchParams }: { searchParams?: { [key: string]: string | string[] } }) {
-  const searchQuery = Array.isArray(searchParams?.search) ? searchParams?.search[0] : searchParams?.search ?? "";
-  const searchQueryById = Array.isArray(searchParams?.id) ? searchParams?.id[0] : searchParams?.id ?? "";
+export default function CardsPage({ searchParams }: any) {
+  const searchQuery = searchParams?.search ?? "";
+  const searchQueryById = searchParams?.id ?? "";
 
   return (
     <LayoutContainer>
