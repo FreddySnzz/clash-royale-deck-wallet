182caf282c3c0c698372ec5df6a41ad084ea3e95
FreddySnzz <fredsonsantos@seduc.pi.gov.br>
2025-09-30 18:27:35 -0300

    feat: added real card id to link example

--- src/components/layout/DeckContainer.tsx
+++ src/components/layout/DeckContainer.tsx
@@ -2,7 +2,7 @@ import { GenerateDeckLink } from "@/data/functions/generateDeckLink"
 import Link from "next/link"
 
 export default function DeckContainer() {
-  const link = GenerateDeckLink({ cardIds: [26000000, 26000001, 26000002, 26000003, 26000004, 26000005, 26000006, 26000007], idCardTower: 27000000 });
+  const link = GenerateDeckLink({ cardIds: [26000000, 26000001, 26000002, 26000003, 26000004, 26000005, 26000006, 26000007], idCardTower: 159000002 });
 
   return (
     <div className="grid grid-cols-8 p-2 bg-slate-700">
